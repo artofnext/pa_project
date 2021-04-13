682d6096bab83661d36cbdb339b9767a4e31ca9e
artofnext <novnovalex@gmail.com>
2021-04-13 15:28:33 +0100

    Visitor implemented in first stage

--- src/main/kotlin/gson_generator/Classes.kt
+++ src/main/kotlin/gson_generator/Classes.kt
@@ -1,13 +1,49 @@
 package gson_generator
 
 abstract class Jvalue {
-    open fun accept(v: Visitor) {
-        v.visit(this)
-    }
+    abstract fun accept(v: Visitor)
 }
 
 interface Visitor {
-    fun visit(node: Jvalue) {}
+
+    fun visit(value: Jnode)
+    fun visit(value: Jobject)
+    fun visit(value: Jarray)
+    fun visit(value: Jstring)
+    fun visit(value: Jnumber)
+    fun visit(value: Jbool)
+    fun visit(value: Jnull)
+}
+
+class StringifyVisitor : Visitor {
+    var str = ""
+    override fun visit(node: Jnode) {
+        str += " ${node.key}: "
+    }
+
+    override fun visit(obj: Jobject) {
+        str += "{ "
+    }
+
+    override fun visit(arr: Jarray) {
+        str += "[ "
+    }
+
+    override fun visit(value: Jstring) {
+        str += '"' + value.value + '"'
+    }
+
+    override fun visit(value: Jnumber) {
+        str += value.value.toString()
+    }
+
+    override fun visit(value: Jbool) {
+        str += value.value.toString()
+    }
+
+    override fun visit(value: Jnull) {
+        str += "null"
+    }
 }
 
 class Jnode (key: String = "root", value: Jvalue): Jvalue() {
@@ -16,6 +52,7 @@ class Jnode (key: String = "root", value: Jvalue): Jvalue() {
 
     override fun accept(v: Visitor) {
         v.visit(this)
+        value.accept(v)
     }
 
     override fun toString(): String {
@@ -32,6 +69,11 @@ class Jobject (var value: MutableList<Jnode>): Jvalue() {
         value.add(elem)
     }
 
+    override fun accept(v: Visitor) {
+        v.visit(this)
+        value.forEach { it.accept(v) }.also { println("Jobject accept visitor") }
+    }
+
     override fun toString(): String {
         var str = "{ "
         str += value.joinToString(", ") {it.toString()}
@@ -46,6 +88,11 @@ class Jarray (var value: MutableList<Jvalue>): Jvalue() {
         value.add(elem)
     }
 
+    override fun accept(v: Visitor) {
+        v.visit(this)
+        value.forEach { it.accept(v) }
+    }
+
     override fun toString(): String {
         var str = "[ "
         str += value.joinToString(", ") {it.toString()}
@@ -56,12 +103,20 @@ class Jarray (var value: MutableList<Jvalue>): Jvalue() {
 
 class Jstring(var value: String): Jvalue() {
 
+    override fun accept(v: Visitor) {
+        v.visit(this)
+    }
+
     override fun toString(): String {
         return '"' + value + '"'
     }
 }
 
 class Jnumber (var value: Double): Jvalue() {
+
+    override fun accept(v: Visitor) {
+        v.visit(this)
+    }
 
     override fun toString(): String {
         return value.toString()
@@ -70,12 +125,20 @@ class Jnumber (var value: Double): Jvalue() {
 
 class Jbool (var value: Boolean): Jvalue() {
 
+    override fun accept(v: Visitor) {
+        v.visit(this)
+    }
+
     override fun toString(): String {
         return value.toString()
     }
 }
 
 class Jnull: Jvalue() {
+
+    override fun accept(v: Visitor) {
+        v.visit(this)
+    }
 
     override fun toString(): String {
         return "null"
@@ -106,12 +169,12 @@ fun main() {
     var rootNode1 = Jnode(value = rootObj)
     var rootNode2 = Jnode(value = rootArray)
 
-    println(myArray)
-    println(myObj)
-    println(myNode1)
-    println("root node is object")
-    println(rootNode1)
-    println("root node is array")
-    println(rootNode2)
+    val jsonStr = rootNode1.toString()
+    println(jsonStr)
+
+    val myStrVisitor = StringifyVisitor()
+
+    rootNode1.accept(myStrVisitor)
+    println(myStrVisitor.str)
 
 }
